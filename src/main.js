118357c3993634ee9732d8aaab4da0b068091418
cmb347827 <v38d422ll621dem@gmail.com>
2025-12-18 11:46:46 -0500

    added date-fns

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import { addDays } from "date-fns";
 
 document.querySelector('#app').innerHTML = `
   <div>
